bb364488ff91e6e3c9ccd43e56d15e04e1829c4d
Panha <yoeunsopanha2001@gmail.com>
2021-12-07 20:21:46 +0700

    v2

--- TableView_Video/TableView_Video/Video.swift
+++ TableView_Video/TableView_Video/Video.swift
@@ -16,7 +16,7 @@ struct Video {
     static func fetchVideos() -> [Video] {
         let v1 = Video(authorName: "Panha", videoFileName: "v1", description: "This is an example of local video.", thumbnailFileName: "v1")
         let v2 = Video(authorName: "Tola", videoFileName: "v2", description: "This is an example of local video.", thumbnailFileName: "v2")
-        let v3 = Video(authorName: "Panha", videoFileName: "v3", description: "This is an example of local video.", thumbnailFileName: "v3")
+        let v3 = Video(authorName: "Seanghay", videoFileName: "v3", description: "This is an example of local video.", thumbnailFileName: "v3")
         let v4 = Video(authorName: "Panha", videoFileName: "v4", description: "This is an example of local video.", thumbnailFileName: "v4")
         let v5 = Video(authorName: "Panha", videoFileName: "v5", description: "This is an example of local video.", thumbnailFileName: "v5")
         let v6 = Video(authorName: "Panha", videoFileName: "v6", description: "This is an example of local video.", thumbnailFileName: "v6")
